f106239a971be7d5a858a26800fa0d20768cd134
Mayur Kapadi <125800989+mkkapadi12@users.noreply.github.com>
2025-10-28 21:50:38 +0530

    ready to set up project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-red-500">
+      Hello App
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default App;
